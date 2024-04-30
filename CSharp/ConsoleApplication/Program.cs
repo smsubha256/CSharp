90ac5cfdeff8df6c49f5fcc6dc8b7627270c6985
smsubha256 <smsubha256@gmail.com>
2024-04-30 18:28:55 -0500

    Add files via upload

---
+++ CSharp/ConsoleApplication/Program.cs
@@ -0,0 +1,113 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Net.WebSockets;
+using System.Reflection.Metadata;
+
+namespace ListWorkouts;
+
+public class PersonInfo
+{
+    private  string userID;
+    public required string UserID
+    {
+        get
+        {
+            return convertAllUppercase(this.userID ?? "user id was empty");
+        }
+        set => this.userID = value;
+    }
+    private string firstName;
+    public string FirstName
+    {
+        get => this.firstName;
+        set => this.firstName = value;
+    }
+    private string lastName;
+    public string LastName
+    {
+        get => this.lastName;
+        set => this.lastName = value;
+    }
+    private string email;
+    public string Email
+    {
+        get => this.email;
+        set => this.email = value;
+    }
+
+    private string convertAllUppercase(string value)
+    {
+        if (value == null) return value;
+        return value.ToUpper();
+    }
+}
+interface IUserList
+{
+    List<PersonInfo> GetAllUsers();
+    PersonInfo GetUserByID();
+}
+class DisplayUser : IUserList
+{
+    List<PersonInfo> users = new List<PersonInfo>();
+  
+    public List<PersonInfo> GetAllUsers()
+    {
+       users = new List<PersonInfo>();
+        if (users != null && users.Count > 0)
+        {
+            return users;
+        }
+        
+            
+        users = new List<PersonInfo> {
+        new PersonInfo{UserID="abc", Email="abc@gmail.com", FirstName="A"},
+        new PersonInfo{UserID="def", Email="def@gmail.com", LastName="EF"},
+        new PersonInfo{UserID="ghi", Email="ghi@gmail.com", FirstName="G", LastName="HI"},
+        new PersonInfo{UserID="jkl", Email="jkl@gmail.com", FirstName="J", LastName="KL"},
+        new PersonInfo{UserID="mno", Email="mno@gmail.com", FirstName="M", LastName="NO"}
+        };
+        return users;
+    }
+
+    public PersonInfo GetUserByID()
+    { 
+        Console.WriteLine("Enter the User ID");
+        string id = Console.ReadLine();
+        if (string.IsNullOrEmpty(id))
+        {
+            Console.WriteLine("please enter valid iuser id");
+                this.GetUserByID();
+        }
+        PersonInfo userdetails = users.FirstOrDefault(user => user.UserID.ToLowerInvariant() == id.ToLowerInvariant());
+        List<PersonInfo> userdetails1 = users.Where(user => user.UserID.ToLowerInvariant() == id.ToLowerInvariant()).ToList();
+        userdetails = userdetails1.FirstOrDefault();
+        return userdetails;  
+    }
+        
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        IUserList userList = new DisplayUser();
+
+        List<PersonInfo> users = userList.GetAllUsers();
+        foreach (PersonInfo user in users)
+        {
+            Console.WriteLine (user.Email, user.FirstName, user.LastName, user.UserID);
+        }
+        PersonInfo userInfo = userList.GetUserByID();
+
+        var ttt = "id " + userInfo.UserID + " | " + userInfo.Email;
+        ttt = $"id  user id is { userInfo.UserID }  emssil is: {userInfo.Email}";
+       
+        Console.WriteLine("The User details are: " + ttt);
+        
+        Console.WriteLine("The User details are: {0} {1}", userInfo.UserID, userInfo.FirstName);
+
+    }
+}
+
+
+
+
---
+++ CSharp/ListOperations/Program.cs
@@ -0,0 +1,106 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*class Listfunctions
+{
+    private List<string> fruits = new List<string>();
+    public List<string> Fruits
+    {
+        get => this.fruits; 
+        set => this.fruits = value;
+    }
+}*/
+using System.Globalization;
+
+interface IFruitList
+{
+    List<string> AddFruits();
+    List<string> RemoveFruits();    
+}
+class FruitList : IFruitList
+{
+    List<string> fruits = new List<string>();
+    public List<string> AddFruits()
+    {
+        Console.WriteLine("Do you want to add fruits to the list? (y/n)");
+        string a = Console.ReadLine();
+        if (a == "y")
+        {
+            Console.WriteLine("enter a fruit to add:");
+            string addfruit = Console.ReadLine();
+            if (string.IsNullOrEmpty(addfruit))
+            {
+                Console.WriteLine("Enter a valid fruit name:");
+                this.AddFruits();
+
+            }
+            fruits.Add(addfruit);
+            //fruits.Add(addfruit);
+            //fruits.Add(addfruit);
+            this.AddFruits();
+            
+        }
+        return fruits;
+    }
+    public List<string> RemoveFruits() {
+        Console.WriteLine("Do you want to remove any fruit? (y/n)");
+        string a = Console.ReadLine();
+        if (a == "y")
+        {
+            Console.WriteLine("enter fruit to remove:");
+            string removefruit = Console.ReadLine();
+            if (!fruits.Contains(removefruit) || string.IsNullOrEmpty(removefruit))
+            {
+                Console.WriteLine("Enter a valid fruit name");
+                this.RemoveFruits();
+            }
+
+            fruits.Remove(removefruit);
+        }
+        return fruits; 
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+      FruitList f = new FruitList();
+        List<string> newfruitlist = f.AddFruits();
+        foreach(string fruit in newfruitlist) { Console.WriteLine(fruit); }
+        newfruitlist = f.RemoveFruits();
+        foreach (string fruit in newfruitlist) { Console.WriteLine(fruit); }
+        List<int> numbers = new List<int>();
+        numbers.Add(1);
+        numbers.Add(2);
+        numbers.Add(3);
+        numbers.Add(4);
+        numbers.Add(5);
+        numbers.Add(6);
+        numbers.Add(7);
+        numbers.Add(8);
+        numbers.Add(9);
+        numbers.Add(10);
+        int b = numbers.Where(n => n % 2 == 0).FirstOrDefault();
+        Console.WriteLine("first or default value:"+b);
+        int c = numbers.Where(n => n % 2 == 0).LastOrDefault();
+        Console.WriteLine("Last or default:{0}", c);
+        int last = numbers.Last();
+        Console.WriteLine("last nunber:"+ last);
+        var num = numbers.Select(n=>n*n);
+        Console.WriteLine("Squares of the numbers:");
+        foreach (var n in num)
+        {
+            Console.WriteLine(n);
+        }
+        List<List<string>> nestedlist = new List<List<string>> { new List<string> { "a", "b" } ,
+        new List<string>{"c", "d" },
+        new List<string>{"e", "f"} };
+        var flatlist = nestedlist.SelectMany(list => list);
+        Console.WriteLine("flat list:");
+        foreach (var value in flatlist)
+        {
+            Console.WriteLine(value);
+        }
+
+    }
+    
+    
+}
